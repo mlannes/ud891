f885ee49af1edd9abc574b895b6efe81f17be23b
mlannes <mlannes@users.noreply.github.com>
2017-06-30 16:33:38 -0400

    Update checkboxes.js

--- lesson5-semantics-aria/03-first-steps/checkboxes.js
+++ lesson5-semantics-aria/03-first-steps/checkboxes.js
@@ -37,12 +37,13 @@
     if (this.el.hasAttribute('checked')) {
       this.el.removeAttribute('checked');
 
-      // Hmm.
+      this.el.setAttribute('aria-checked', 'false');
 
     } else {
       this.el.setAttribute('checked', '');
 
-      // Hmmmmm.
+      
+      this.el.setAttribute('aria-checked', 'true');
 
     }
   };
